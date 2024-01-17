9a1de9ad0def447aac97b348794787b75067ef2b
Vlad <vladmen13@mail.ru>
2024-01-17 19:43:27 +0300

    test2

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,26 +1,26 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  parserOptions: {
+    ecmaVersion: 2021,
+  },
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: [
+        ".eslintrc.{js,cjs}",
+      ],
+      parserOptions: {
+        sourceType: "script",
+      },
     },
-    "overrides": [
-        {
-            "env": {
-                "node": true
-            },
-            "files": [
-                ".eslintrc.{js,cjs}"
-            ],
-            "parserOptions": {
-                "sourceType": "script"
-            }
-        }
-    ],
-    "parserOptions": {
-        "ecmaVersion": "latest"
-    },
-    "rules": {
-      "no-console": "off",
-      "brace-style": ["error", "1tbs", { "allowSingleLine": true }]
-    }
-}
+  ],
+  rules: {
+    "no-console": "off",
+    "brace-style": ["error", "1tbs", { "allowSingleLine": true }],
+  },
+};
